a9cff9ce543139ec13ddb697dcc99a65efd555cf
AlphaFlow229 <alphaflow249@gmail.com>
2026-08-10 00:39:58 +0600

    Feat: Homework 5 complete

---
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -0,0 +1,28 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Bookmark
+    {
+        private Guid _id;
+        private Guid _tweetId;
+        private DateTime _bookMarkAt;
+
+
+        public Guid UserId
+        {
+            get { return _id; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+
+        public DateTime BookMarkAt
+        {
+            get { return _bookMarkAt; }
+        }
+
+
+    }
+}
